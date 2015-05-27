e0d0f06b1000d134f7ab651f73798f331f526d5a
jparsaie <jparsaie@turbine.com>
2015-05-27 14:42:01 -0400

    Updated with comments

--- d_clean.py
+++ d_clean.py
@@ -4,21 +4,29 @@ import os
 import shutil
 import datetime
 
-def get_distro_list(mainDirectory):
+#Provided a directory to a service, gather all subdirectories for all
+#distros in all of the revisions for the service. Return that list.
+def get_distro_list(serviceDirectory):
     service_distros = []
-    revision_list = os.listdir(mainDirectory)
+    revision_list = os.listdir(serviceDirectory)
 
+    #Iterate through revisions in service. Store all distros in all
+    #revisions into service_distros.
     for r in revision_list:
-        revision_distros = os.listdir(os.path.join(mainDirectory, r))
+        revision_distros = os.listdir(os.path.join(serviceDirectory, r))
 
         for rd in revision_distros:
             service_distros.append(os.path.join(r, rd))
 
+    ##Sort and reverse the list for proper analysis in get_keep_discard_list()
     service_distros.sort()
     service_distros.reverse()
 
     return service_distros
 
+#Provided a list of all distros in a service, return a list of
+#distros directories that are kept, and a list of distros that
+#are discarded.
 def get_keep_discard_list(distroList, serviceDirectory):
     keep = []
     discard = []
@@ -26,23 +34,30 @@ def get_keep_discard_list(distroList, serviceDirectory):
     for i in range(0, len(distroList)):
         distro = distroList[i]
         action = "KEEP"
+
+        #Removal threshold set at 5.
         if(i < 5):
             keep.append(distro)
         else:
             discard.append(distro)
             action = "DISCARD"
 
+        #Log the action taken for the distro, stamping its
+        #location, size, action taken, and time of action.
         log(os.path.join(serviceDirectory, distro), get_file_size(os.path.join(serviceDirectory, distro)), action)
 
     print '\n'
     return keep, discard
 
-def log(serviceDirectory):
-    print "Cleaning up service [" + serviceDirectory.upper() + "]..."
-
+#Provided a distro directory, distro size, and action taken
+#on distro, output a log to both the console and a .txt file
+#outlining the mentioned parameters, along with a time of action.
 def log(distro, distroSize, action):
+
+    #Get current time.
     now = datetime.datetime.now()
 
+    #Write to file.
     with open("test.txt", "a") as testFile:
         testFile.write("[" + distro + "]" \
                        "[" + str("%.2f" % distroSize) + "mb]" \
@@ -50,12 +65,14 @@ def log(distro, distroSize, action):
                        "[" + str(now.month) + '/' + str(now.day) + '/' + str(now.year) + "" \
                        " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + "]\n")
 
+    #Output to console.
     print "\t[" + distro + "]" \
            "[" + str("%.2f" % distroSize) + "mb]" \
            "[" + action + "]"\
            "[" + str(now.month) + '/' + str(now.day) + '/' + str(now.year) + "" \
            " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + "]"
 
+#Provided the directory of a file, return its size in MB.
 def get_file_size(fileDirectory):
     total_size = 0
 
--- test_clean.py
+++ test_clean.py
@@ -8,16 +8,24 @@ import d_clean
 distroDirectory = "testDistro"
 
 class TestStringMethods(unittest.TestCase):
+
+    #Called before the beginning of every test.
     @classmethod
     def setUpClass(cls):
         cls.cwd = "E:\/"
+
+        #Remove old main distro directory and create a new one.
         shutil.rmtree(os.path.join(cls.cwd, distroDirectory), ignore_errors=True)
         os.mkdir(os.path.join(cls.cwd, distroDirectory))
 
+    #Called at the end of entire test run.
     @classmethod
     def tearDown(cls):
+        #Remove main distro directory after suite run complete.
         shutil.rmtree(os.path.join("E:\/", distroDirectory), ignore_errors=True)
 
+    #Provided a version directory and list of distros, return a Boolean
+    #value specifying if given version exists in list.
     def version_in_distro_list(self, version, distro_list):
         for d in distro_list:
             if version in d.encode('string-escape'):
@@ -25,16 +33,20 @@ class TestStringMethods(unittest.TestCase):
         return False
 
     def test_minimal(self):
+        #Set up service(s), revision(s), and distro(s)
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.0", "1.0"))
 
+        #Get list of all distros, and list of distros kept/discarded
         distros = d_clean.get_distro_list(os.path.join(self.cwd, distroDirectory, "service"))
         keep, discard = d_clean.get_keep_discard_list(distros, os.path.join(self.cwd, distroDirectory, "service"))
 
+        #Verify that one distro is kept (1.0), and none were discarded
         self.assertEqual(1, len(keep))
         self.assertIn('1.0', distros[0])
         self.assertEqual(0, len(discard))
 
     def test_basic_one_major_revision(self):
+        #Set up service(s), revision(s), and distro(s)
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.0", "1.0"))
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.0", "1.2"))
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.0", "1.4"))
@@ -42,9 +54,12 @@ class TestStringMethods(unittest.TestCase):
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.0", "1.8"))
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.0", "1.13"))
 
+        #Get list of all distros, and list of distros kept/discarded
         distros = d_clean.get_distro_list(os.path.join(self.cwd, distroDirectory, "service"))
         keep, discard = d_clean.get_keep_discard_list(distros, os.path.join(self.cwd, distroDirectory, "service"))
 
+        #Verify that 5 distros kept, and 1 discarded.
+        #Verify that correct distros are in their respected lists.
         self.assertEqual(5, len(keep))
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.13', keep))
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.8', keep))
@@ -55,6 +70,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.0', discard))
 
     def test_basic_multiple_major_revision(self):
+        #Set up service(s), revision(s), and distro(s)
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.0", "1.0"))
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.0", "1.4"))
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.0", "1.5"))
@@ -62,9 +78,13 @@ class TestStringMethods(unittest.TestCase):
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.1", "1.3"))
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service", "1.1", "1.4"))
 
+
+        #Get list of all distros, and list of distros kept/discarded
         distros = d_clean.get_distro_list(os.path.join(self.cwd, distroDirectory, "service"))
         keep, discard = d_clean.get_keep_discard_list(distros, os.path.join(self.cwd, distroDirectory, "service"))
 
+        #Verify that 5 distros kept, and 1 discarded.
+        #Verify that correct distros are in their respected lists.
         self.assertEqual(5, len(keep))
         self.assertTrue(self.version_in_distro_list(r'1.1\\1.4', keep))
         self.assertTrue(self.version_in_distro_list(r'1.1\\1.3', keep))
@@ -75,6 +95,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.0', discard))
 
     def test_basic_multiple_services(self):
+        #Set up service(s), revision(s), and distro(s)
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service1", "1.0", "1.0"))
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service1", "1.0", "1.2"))
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service1", "1.0", "1.4"))
@@ -89,9 +110,13 @@ class TestStringMethods(unittest.TestCase):
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service2", "1.0", "1.32"))
         os.makedirs(os.path.join(self.cwd, distroDirectory, "service2", "1.0", "1.48"))
 
+
+        #Get list of all distros, and list of distros kept/discarded
         distros = d_clean.get_distro_list(os.path.join(self.cwd, distroDirectory, "service1"))
         keep, discard = d_clean.get_keep_discard_list(distros, os.path.join(self.cwd, distroDirectory, "service1"))
 
+        #Verify that 5 distros kept, and 1 discarded for service1.
+        #Verify that correct distros are in their respected lists for service1.
         self.assertEqual(5, len(keep))
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.12', keep))
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.8', keep))
@@ -101,10 +126,12 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(1, len(discard))
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.0', discard))
 
-
+        #Get list of all distros, and list of distros kept/discarded
         distros = d_clean.get_distro_list(os.path.join(self.cwd, distroDirectory, "service2"))
         keep, discard = d_clean.get_keep_discard_list(distros, os.path.join(self.cwd, distroDirectory, "service2"))
 
+        #Verify that 5 distros kept, and 1 discarded for service2.
+        #Verify that correct distros are in their respected lists for service2.
         self.assertEqual(5, len(keep))
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.11', keep))
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.12', keep))
@@ -113,6 +140,19 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.48', keep))
         self.assertEqual(1, len(discard))
         self.assertTrue(self.version_in_distro_list(r'1.0\\1.0', discard))
+
+    def test_no_minor(self):
+        #Set up service(s), revision(s), and distro(s)
+        os.makedirs(os.path.join(self.cwd, "testDistro", "service"))
+        os.makedirs(os.path.join(self.cwd, "testDistro", "service", "1.0"))
+
+        #Get list of all distros, and list of distros kept/discarded
+        distros = d_clean.get_distro_list(os.path.join(self.cwd, distroDirectory, "service"))
+        keep, discard = d_clean.get_keep_discard_list(distros, os.path.join(self.cwd, distroDirectory, "service"))
+
+        #Verify that keep and discard are empty. No results.
+        self.assertEqual(0, len(keep))
+        self.assertEqual(0, len(discard))
 
 if __name__ == '__main__':
     unittest.main()
